c716803505084c7620f312d4589b924bdf75a2e0
Nick Rempel <nbrempel@gmail.com>
2025-11-05 09:21:36 -0800

    Point bundled cask at official repo

--- homebrew/Casks/wt.rb
+++ homebrew/Casks/wt.rb
@@ -2,10 +2,10 @@ cask "wt" do
   version "0.1.0"
   sha256 "5cc8003a5bcc743dcfcd09a9a62479f83eb01db66c4dc29d552f3cfceb022265"
 
-  url "https://github.com/USERNAME/REPO/releases/download/v#{version}/wt-#{version}.zip"
+  url "https://github.com/nrempel/wt/releases/download/v#{version}/wt-#{version}.zip"
   name "wt"
   desc "Friendly wrapper around git worktrees"
-  homepage "https://github.com/USERNAME/REPO"
+  homepage "https://github.com/nrempel/wt"
 
   binary "wt"
 end
